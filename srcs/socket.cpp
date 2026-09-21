ac313792f82144061a476adeb7de98216937022a
Felix Benech <fbenech@c2r3p2.42lehavre.fr>
2026-09-21 23:10:01 +0200

    begining the project

--- srcs/socket.cpp
+++ srcs/socket.cpp
@@ -1,2 +1,19 @@
 #include "../headers/socket.hpp"
 
+void socket()
+{
+	int fd;
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd == -1)
+		/*throw une erreur*/;
+	try
+	{
+		int opt = 1;
+		setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, 1);
+	}
+	catch (const std::exception)
+	{
+		close(fd);
+		/*throw une erreur "setsockopt failed"*/;
+	}
+}
